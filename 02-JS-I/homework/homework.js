2b177ad6fe0bf02a6d0e371a0c197dfde4f251fa
Jhon Lesvis Moreno Perea <jhon.moreno@fonvalmed.local>
2022-09-02 23:11:42 -0500

    Primer commit 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -239,7 +239,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  var letra = prompt('Ingrese vocal: ');
+  //var letra = prompt('Ingrese vocal: ');
   if (letra.lenght > 1) {
     return "Dato incorrecto";
   } else if (letra == 'a', 'e', 'i', 'o', 'u') {
