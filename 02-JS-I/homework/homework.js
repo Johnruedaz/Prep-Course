9cb103d9a6d4df0a565355bf01a9685b84f3450d
Jhon Lesvis Moreno Perea <jhon.moreno@fonvalmed.local>
2022-09-02 21:51:46 -0500

    Primer commit 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -185,7 +185,6 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  var str = "hello world";
   var carácter = "!";
   return str.concat(carácter);
 }
