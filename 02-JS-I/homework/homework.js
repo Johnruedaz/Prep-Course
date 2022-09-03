404ce025ca107a2a97ec7000be2f4038a4703bdd
Jhon Lesvis Moreno Perea <jhon.moreno@fonvalmed.local>
2022-09-02 21:34:35 -0500

    Primer commit 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -75,7 +75,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.equals(str2)) {
+  if (str1 == str2) {
     return true;
   }
   return false;
