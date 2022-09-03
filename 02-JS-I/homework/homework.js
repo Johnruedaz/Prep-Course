e869dfb35114591669f2b0eaa82330fef5615d4a
Jhon Lesvis Moreno Perea <jhon.moreno@fonvalmed.local>
2022-09-02 20:34:29 -0500

    Primer commit 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -85,6 +85,7 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  var num = 100
   if (num < 90) {
     return true;
   }
